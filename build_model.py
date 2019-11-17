630a521fdaf643666d4d9828eaaad2201b3f05ac
fmusgrove <forrestmusgrove@icloud.com>
2019-11-16 23:48:00 -0600

    Refactored model build script to work with TensorFlow 1.15.0 and changed model structure to approximate that of the NVIDIA DAVE-2 system

--- build_model.py
+++ build_model.py
@@ -1,86 +1,109 @@
 import os
 import pickle
 
+import numpy as np
 import tensorflow as tf
 
-# Support running in a Colab TPU accelerated runtime if available
-TPU_MODE = True if os.environ.get('COLAB_TPU_ADDR') else False
-
-if TPU_MODE:
-    PICKLE_DATA_DIR = '/content/'
-else:
-    PICKLE_DATA_DIR = '../res/TrainingData/compressed/'
-
-batch_size = 39
+# Batch size should be a divisor of number of training samples
+batch_size = 181
 num_epochs = 8
+
+print(f'Running on TensorFlow v.{tf.__version__} with Numpy v.{np.__version__}')
 
 print('>  ====== Loading compressed training data')
 
 # Load processed compressed data from disk
-pickle_in = open(PICKLE_DATA_DIR + 'x_train.pickle', 'rb')
+pickle_in = open('x_train.pickle', 'rb')
 x_train = pickle.load(pickle_in)
 
-pickle_in = open(PICKLE_DATA_DIR + 'y_train.pickle', 'rb')
+pickle_in = open('y_train.pickle', 'rb')
 y_train = pickle.load(pickle_in)
 
-pickle_in = open(PICKLE_DATA_DIR + 'x_valid.pickle', 'rb')
+pickle_in = open('x_valid.pickle', 'rb')
 x_valid = pickle.load(pickle_in)
 
-pickle_in = open(PICKLE_DATA_DIR + 'y_valid.pickle', 'rb')
+pickle_in = open('y_valid.pickle', 'rb')
 y_valid = pickle.load(pickle_in)
 
 print('>  ====== Compressed training data loaded')
 
-model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.BatchNormalization(input_shape=x_train.shape[1:]))
-model.add(tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='elu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.Dropout(0.1))
+def define_model():
+    model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.BatchNormalization(input_shape=x_train.shape[1:]))
-model.add(tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='elu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.Dropout(0.1))
+    # Convolutional feature map layers
+    model.add(tf.keras.layers.BatchNormalization(input_shape=x_train.shape[1:]))
+    model.add(tf.keras.layers.Conv2D(24, (5, 5), padding='same', activation='elu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(0.1))
 
-model.add(tf.keras.layers.BatchNormalization(input_shape=x_train.shape[1:]))
-model.add(tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='elu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.Dropout(0.1))
+    model.add(tf.keras.layers.BatchNormalization(input_shape=x_train.shape[1:]))
+    model.add(tf.keras.layers.Conv2D(36, (5, 5), padding='same', activation='elu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(0.1))
 
-model.add(tf.keras.layers.Flatten())  # Convert the 3D feature maps to 1D feature vectors
-model.add(tf.keras.layers.Dense(32))
-model.add(tf.keras.layers.Activation('elu'))
-model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.BatchNormalization(input_shape=x_train.shape[1:]))
+    model.add(tf.keras.layers.Conv2D(48, (5, 5), padding='same', activation='elu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(0.1))
 
-model.add(tf.keras.layers.Dense(1))
-model.add(tf.keras.layers.Activation('softmax'))
+    model.add(tf.keras.layers.BatchNormalization(input_shape=x_train.shape[1:]))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='elu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(0.1))
 
-if TPU_MODE:
-    tpu_model = tf.contrib.tpu.keras_to_tpu_model(
-        model,
-        strategy=tf.contrib.tpu.TPUDistributionStrategy(
-            tf.contrib.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
-        )
-    )
+    model.add(tf.keras.layers.BatchNormalization(input_shape=x_train.shape[1:]))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='elu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(0.1))
 
-    tpu_model.compile(
-        optimizer=tf.keras.optimizers.Adam(lr=1e-3),
-        loss=tf.keras.losses.categorical_crossentropy,
-        metrics=['accuracy']
-    )
+    # Convert the 3D feature maps to 1D feature vectors
+    model.add(tf.keras.layers.Flatten())
 
-    tpu_model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data=(x_valid, y_valid))
+    # Fully-connected layers
+    model.add(tf.keras.layers.Dense(100))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.Dropout(0.2))
 
-    cpu_model = tpu_model.sync_to_cpu()
-    cpu_model.save('optimized_classification_graph.model')
-else:
+    model.add(tf.keras.layers.Dense(50))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    model.add(tf.keras.layers.Dense(10))
+    model.add(tf.keras.layers.Activation('elu'))
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    # Output layer: normalized steering angle
+    model.add(tf.keras.layers.Dense(1))
+    model.add(tf.keras.layers.Activation('softmax'))
+
+    return model
+
+
+# Obtain a resolver and connect to a TPU runtime
+resolver = tf.contrib.cluster_resolver.TPUClusterResolver('grpc://' + os.environ['COLAB_TPU_ADDR'])
+tf.contrib.distribute.initialize_tpu_system(resolver)
+strategy = tf.contrib.distribute.TPUStrategy(resolver)
+
+# Define and compile the model
+with strategy.scope():
+    model = define_model()
+
     model.compile(
         optimizer=tf.keras.optimizers.Adam(lr=1e-3),
-        loss=tf.keras.losses.categorical_crossentropy,
-        metrics=['accuracy']
+        loss='mse',
+        metrics=['mse', 'mae']
     )
-    model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data=(x_valid, y_valid))
-    model.save('optimized_classification_graph.model')
+
+# Train the model
+model.fit(
+    x_train.astype(np.float32),
+    y_train.astype(np.float32),
+    epochs=num_epochs,
+    batch_size=batch_size,
+    validation_data=(x_valid.astype(np.float32), y_valid.astype(np.float32))
+)
+
+model.save_weights('jetCNN.h5', overwrite=True)
 
 print('>>>>>>> Training complete')
